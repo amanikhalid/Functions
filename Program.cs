89f2ec1929256c8a75e9daf32b13502e0a8314b0
Amani Khalid <amanikhalidd@gmail.com>
2025-04-09 14:50:59 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Functions
+{
+    internal class Program
+    {
+        static double[] marks = new double[10];
+        static int[] Ages = new int[10];
+        static string[] names = new string[10];
+        static DateTime[] dates = new DateTime[10];
+        static int StudentCounter = 0;
+
+        static void Main(string[] args)
+        {
+            int choice = 0; //Declare and initialize choice
+            while (true)
+            {
+                try //handle the exception if the user enter invalid input
+                {
+                    //Menu System
+                    Console.Clear();
+                    Console.WriteLine("\nSelect a Program:");
+                    Console.WriteLine("1. Even or Odd check");
+                    Console.WriteLine("2. Largest of Three Numbers");
+                    Console.WriteLine("3. Temperature Converter");
+                    Console.WriteLine("4. Simple Discount Calculator");
+                    Console.WriteLine("5. Grading System");
+                    Console.WriteLine("6. Swap Two Numbers");
+                    Console.WriteLine("7. Days to Weeks and Days Converter");
+                    Console.WriteLine("8. Electricity Bill Calculator");
+                    Console.WriteLine("9. Simple Calculator");
+                    Console.WriteLine("10. Exit the system");
+
+                    Console.Write("Enter your choice : ");
+                    choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1: EvenOdd(); break;
+                        case 2: LargestOfThreeNumbers(); break;
+                        case 3: TemperatureConverter(); break;
+                        case 4: SimpleDiscountCalculator(); break;
+                        case 5: GradingSystem(); break;
+                        case 6: SwapTwoNumbers(); break;
+                        case 7: DaysToWeeksAndDaysConverter(); break;
+                        case 8: ElectricityBillCalculator(); break;
+                        case 9: SimpleCalculator(); break;
+                        case 10: return;
+
+                        default: Console.WriteLine("Invalid Choice! Try again."); break;
+                    }
+                    Console.WriteLine("Press any key  "); //ask user to press any key to continue
+                    Console.ReadLine(); //read the user input
+
+                }
+                catch (Exception e)//show exception message if the user enter invalid input
+                {
+                    Console.WriteLine(e.Message);
+
+
+                    Console.WriteLine("Invalid Choice! Try again.");
+                    Console.WriteLine("Press any key  "); //ask user to press any key to continue
+                    Console.ReadLine(); //read the user inputConsole.ReadLine();
+                }
+
+
+            }
+        }
+        // 1. Even or Odd Check
+        static void EvenOdd()
+        {
+
+        }
+
+        //2. Largest of Three Numbers
+        static void LargestOfThreeNumbers()
+        {
+
+        }
+
+        //3. Temperature Converter
+        static void TemperatureConverter()
+        {
+
+        }
+
+        //4. Simple Discount Calculator
+        static void SimpleDiscountCalculator()
+        {
+
+        }
+
+        //5. Grading System
+        static void GradingSystem()
+        {
+
+        }
+
+        //6. Swap Two Numbers
+        static void SwapTwoNumbers()
+        {
+
+        }
+
+        //7. Days to Weeks and Days Converter
+        static void DaysToWeeksAndDaysConverter()
+        {
+
+        }
+
+        //8.  Electricity Bill Calculator
+        static void ElectricityBillCalculator()
+        {
+
+        }
+
+        //9. Simple Calculator
+        static void SimpleCalculator()
+        {
+
+        }
+    }
+
+}
+
